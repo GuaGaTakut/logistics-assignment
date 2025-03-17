1229a1aae4c9c31a2468bb93c8f184a7c14e0df8
GuaGaTakut <nichosalim@gmail.com>
2025-03-17 23:46:04 +0900

    debugged files

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import AssignmentSubmission from "./AssignmentSubmission";
 
 function App() {
   return (
     <div>
       <h1>Assignment Submission</h1>
+      <p>React is working!</p>  {/* Debugging check */}
       <AssignmentSubmission />
     </div>
   );
