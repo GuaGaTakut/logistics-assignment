0df1ce85e20556b33f7b37129e6144a27e98df4c
GuaGaTakut <nichosalim@gmail.com>
2025-03-17 23:04:02 +0900

    Created main.jsx and submission.jsx

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import AssignmentSubmission from "./AssignmentSubmission";
+
+function App() {
+  return (
+    <div>
+      <h1>Assignment Submission</h1>
+      <AssignmentSubmission />
+    </div>
+  );
+}
+
+export default App;
